a11a81756c4f8c72fd3376a8aaacf91bbd971edc
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-08-27 12:52:57 -0300

    lara

---
+++ aula02.py
@@ -0,0 +1,30 @@
+# # operadores
+# a = int(input('Digite um numero:'))
+# b = int(input('Digite outro numero:'))
+# c = a + b
+# d = a - b
+# e = a / b
+# f = a * b
+# g = a // b
+# h = a ** b
+# i = pow(a, 0.5)
+
+
+# print(f'a soma é {c}')
+# print(f'a subtração é {d}')
+# print(f'a divisão é {e}')
+# print(f'a multiplicação {f}')
+# print(f'a divisão inteira é {g}')
+# print(f'a potencia é {h}')
+# print(f'a raiz é {i}')
+
+# if
+print('seja bem vindo ao sistema de horas')
+hora = int(input("digite a hora:"))
+if hora > 12: 
+ print('boa tarde!!') 
+elif hora > 18:
+ print('boa noite!!')
+else:
+ print('bom dia!!')
+ 
